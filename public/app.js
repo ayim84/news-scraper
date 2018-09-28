811160c9bbc31c5dd5201cf0d02173975b4ab3eb
ayim84 <29932922+ayim84@users.noreply.github.com>
2018-09-27 21:41:22 -0600

    Added notes.

---
+++ public/app.js
@@ -0,0 +1,56 @@
+$.getJSON("/articles", function(data)
+{
+    console.log("Data: ", data);
+    for(var i = 0; i < data.length; i++)
+    {
+        var cardDiv = $("<div class='card'>");
+        var cardTitle = $("<h5 class='card-header text-center'>").text(data[i].headline);
+        var cardBody = $("<div class='card-body'>").text(data[i].summary);
+        var cardButton = $("<button type='button' id='addNote' class='btn btn-primary' data-toggle='modal' data-target='#noteModal'>").text('Add/View Note(s)');
+        cardButton.attr("data-id", data[i]._id);
+        var hr = $("<hr>");
+
+        cardDiv.append(cardTitle);
+        cardDiv.append(cardBody);
+        cardDiv.append(cardButton);
+
+        $("#articles").append(cardDiv);
+        $("#articles").append(hr);
+    }
+});
+
+$(document).on("click", "#addNote", function()
+{
+    $("#noteBody").empty();
+
+    var thisId = $(this).attr("data-id");
+
+    $.get("/articles/" + thisId, function(data)
+    {
+        console.log(data);
+        $("#modalTitle").empty();
+        $("#modalTitle").append(data.headline);
+        $("#noteBody").append("<textarea class='form-control' id='noteText' rows='3'></textarea>");
+        $(".modal-footer").html("<button type='button' id='saveNote' class='btn btn-primary' data-id = '" + data._id + "' data-dismiss='modal'>Save</button>");
+
+        if(data.note)
+        {
+            console.log("poop");
+            $("#noteBody").append("<hr>");
+            $("#noteBody").append(data.note.body);
+        }
+    })
+});
+
+$(document).on("click", "#saveNote", function()
+{
+    var thisId = $(this).attr("data-id");
+
+    $.post("/articles/" + thisId, {body: $("#noteText").val().trim()})
+    .then(function(data)
+    {
+        console.log(data);
+        // $("#noteBody").empty();
+    });
+
+});
---
+++ server.js
@@ -0,0 +1,103 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 8080;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+// mongoose.connect("mongodb://localhost/newsScraper", {useNewUrlParser: true});
+
+app.get("/scrape", function(req, res)
+{
+    axios.get("https://deadspin.com/").then(function(response)
+    {
+        var $ = cheerio.load(response.data);
+
+        $("div.post-wrapper").each(function(i, element)
+        {
+            var result = {};
+
+            var articleContent = $(this).children().children();
+
+            result.headline = articleContent.children("h1.headline").children().text();
+            result.url = articleContent.children("h1.headline").children().attr("href");
+            result.summary = articleContent.children().children("p").text();
+            
+            db.Article.create(result)
+            .then(function(dbArticle)
+            {
+                console.log(dbArticle);
+            })
+            .catch(function(err)
+            {
+                return res.json(err);
+            });
+        });        
+    });
+
+    res.send("Scrape Complete!");
+});
+
+app.get("/articles", function(req, res)
+{
+    db.Article.find({})
+    .then(function(dbArticle)
+    {
+        res.json(dbArticle);
+    })
+    .catch(function(err)
+    {
+        res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res)
+{
+    db.Article.findOne({_id: req.params.id})
+    .populate("note")
+    .then(function(dbArticle)
+    {
+        res.json(dbArticle);
+    })
+    .catch(function(err)
+    {
+        res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res)
+{
+    db.Note.create(req.body)
+    .then(function(dbNote)
+    {
+        return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new:true});
+    })
+    .then(function(dbArticle)
+    {
+        res.json(dbArticle);
+    })
+    .catch(function(err)
+    {
+        res.json(err);
+    });
+});
+
+app.listen(PORT, function()
+{
+    console.log("App running on port " + PORT + ".");
+});
